e1abfcfa8d0bc483a3ab82c93a94342f5415f0d0
Heber <heber1514@hotmail.com>
2023-02-25 17:20:02 -0600

    Nombre del proyecto: PrácticaJPA. Descripción: Versión inicial del proyecto

---
+++ src/main/java/com/devpredator/practicajpa/dao/RestauranteDAO.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.devpredator.practicajpa.dao;
+
+import java.util.List;
+
+import com.devpredator.practicajpa.entity.Restaurante;
+
+/**
+ * @author Heber
+ *
+ */
+public interface RestauranteDAO {
+
+	void guardar(Restaurante restaurante);
+
+	void actualizar(Restaurante restaurante);
+
+	void eliminar(long id);
+
+	List<Restaurante> consultar();
+
+	Restaurante consultarById(Long id);
+
+}
